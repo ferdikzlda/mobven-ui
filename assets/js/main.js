bfdf8d77ee3d9d89bfc87dc5ca87a62b58b395be
Ferdi Kizildag <ferdi.kizildag@mobven.com>
2019-03-18 17:44:31 +0300

    Popuplar yapıldı.

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,6 +8,16 @@ $(function () {
         $(e.target.dataset.btn).addClass('active');
       }
     });
+    $('.sms-code-box .search-input').on('keyup',(e)=>{
+      let value=e.target.value;
+      if(value==''){
+        $('.sms-code-box').removeClass('active');
+        $('.sms-timer').removeClass('error');
+      }else{
+        $('.sms-code-box').addClass('active');
+        $('.sms-timer').addClass('error');
+      }
+    });
     $('#daskSearch').on('click',(e)=>{
       if(e.target.classList.value==='disable-btn') return false;
       $('#daskCheckIcon').addClass('show');
@@ -15,4 +25,5 @@ $(function () {
       $('#daskSearch').removeClass('active');
       $('#daskSearch').text('DEĞİŞTİR');
     });
+    $('#invoiceModal').modal('show');
   })
